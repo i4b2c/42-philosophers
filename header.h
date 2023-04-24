8e91fbe466ab06a77f12d911db2624c0c10081c7
i4b2c <traiguuhh@gmail.com>
2023-04-24 11:18:35 +0200

    .

---
+++ header.h
@@ -0,0 +1,33 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stddef.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct d_mutex
+{
+	int time_to_sleep;
+	int time_to_die;
+	int time_to_eat;
+	int id_philosopher;
+	int time_to_die_reset;
+	int max;
+	pthread_mutex_t mutex;
+	pthread_mutex_t die_mutex;
+	pthread_t thread;
+	struct d_mutex *first_mutex;
+}	t_mutex;
+
+typedef struct d_geral
+{
+	t_mutex *mutex;
+	struct d_geral *next;
+}	t_geral;
+
+#endif
